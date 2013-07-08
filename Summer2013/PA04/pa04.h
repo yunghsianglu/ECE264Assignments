3f9b47acf836178379e79a3559e695efc59add4f
chenyuhao <chen902@purdue.edu>
2013-07-08 17:04:19 -0400

    fixed header file

--- Summer2013/PA04/pa04.h
+++ Summer2013/PA04/pa04.h
@@ -15,7 +15,6 @@ SparseNode * SparseArray_build(int * indicies, int * values, int length);
 void SparseArray_destroy ( SparseNode * array );
 int SparseArray_getMin ( SparseNode * array );
 int SparseArray_getMax ( SparseNode * array );
-SparseNode * SparseArray_set ( SparseNode * array , int index , int value );
 SparseNode * SparseArray_getNode ( SparseNode * array , int index );
 SparseNode * SparseArray_remove ( SparseNode * array , int index );
 SparseNode * SparseArray_copy(SparseNode * array);
