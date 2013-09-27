d168524e84336df57c1157d02ae94afdd89e6ed3
Aaron Michaux <aaron@pageofswords.net>
2013-09-27 11:20:25 -0400

    fixed for PA05

--- Fall2013/PA05/pa05.c
+++ Fall2013/PA05/pa05.c
@@ -8,43 +8,60 @@
 #include "pa05.h"
 int main(int argc, char ** argv)
 {
-  if (argc < 4)
-    {
-      printf("need four file names: type, input, output");
-      return EXIT_FAILURE;
-    }
-  if (strcmp(argv[1], "i") == 0) /* sort integers */
-    {
-      int numInteger = 0;
-      int * arrInteger = NULL;
-      arrInteger = readInteger(argv[2], & numInteger);
-      if (numInteger == 0)
+    if (argc < 4)
 	{
-	  return EXIT_FAILURE;
+	    printf("\n"
+		   "   Usage: ./pa05 <option> <input-file> <output-file>\n"
+		   "\n"
+		   "      <option> can be either 's' for a string sort, or 'i'\n"
+		   "      for an integer sort.\n"
+		   "\n"
+		   );
+	    return EXIT_FAILURE;
 	}
-      printInteger(arrInteger, numInteger);
-      sortInteger(arrInteger, numInteger);
-      printInteger(arrInteger, numInteger);
-      saveInteger(argv[3], arrInteger, numInteger);
-      freeInteger(arrInteger, numInteger);
-      return EXIT_SUCCESS;
-    }
-  if (strcmp(argv[1], "s") == 0) /* sort strings */
-    {
-      int numString = 0;
-      char * * arrString = NULL;
-      arrString = readString(argv[2], & numString);
-      if (numString == 0)
+    if (strcmp(argv[1], "i") == 0) /* sort integers */
 	{
-	  return EXIT_FAILURE;
+	    int numInteger = 0;
+	    int * arrInteger = NULL;
+	    arrInteger = readInteger(argv[2], & numInteger);
+	    if (numInteger == 0)
+		{
+		    return EXIT_FAILURE;
+		}
+	    printf("\n");
+	    printInteger(arrInteger, numInteger);
+	    printf("\n\n   // ~ Printing Sorted Output ~ //\n\n");
+	    sortInteger(arrInteger, numInteger);
+	    printInteger(arrInteger, numInteger);
+	    printf("\n");
+	    saveInteger(argv[3], arrInteger, numInteger);
+	    freeInteger(arrInteger, numInteger);
+	    return EXIT_SUCCESS;
 	}
-      printString(arrString, numString);
-      sortString(arrString, numString);
-      printString(arrString, numString);
-      saveString(argv[3], arrString, numString);
-      freeString(arrString, numString);
-      return EXIT_SUCCESS;
-    }
-  /* unknown type */
-  return EXIT_FAILURE;
+    else if (strcmp(argv[1], "s") == 0) /* sort strings */
+	{
+	    int numString = 0;
+	    char * * arrString = NULL;
+	    arrString = readString(argv[2], & numString);
+	    if (numString == 0)
+		{
+		    return EXIT_FAILURE;
+		}
+	    printf("\n");
+	    printString(arrString, numString);
+	    sortString(arrString, numString);
+	    printf("\n\n   // ~ Printing Sorted Output ~ //\n\n");
+	    printString(arrString, numString);
+	    printf("\n");
+	    saveString(argv[3], arrString, numString);
+	    freeString(arrString, numString);
+	    return EXIT_SUCCESS;
+	}
+    else 
+	{
+	    fprintf(stderr, "Unknown <option>, '%s'. Should be either 's' or 'i'\n",
+		    argv[1]);
+	}
+    /* unknown type */
+    return EXIT_FAILURE;
 }
