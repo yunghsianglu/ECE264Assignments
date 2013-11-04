43cf366cb9fe7c9911173b8182da2223ea6d2305
Aaron Michaux <aaron@pageofswords.net>
2013-11-04 01:58:53 -0500

    PA10 a go

---
+++ Fall2013/PA10/pa10.h
@@ -0,0 +1,27 @@
+#ifndef PA10_H
+#define PA10_H
+
+// ----------------------------------- Stack (Linked List)
+typedef struct list_node_t {
+    int value;
+    struct list_node_t * next;
+} ListNode;
+
+typedef struct stack_t {
+    ListNode * list;
+} Stack;
+
+Stack * Stack_create();
+void Stack_destroy(Stack * stack);
+int Stack_isEmpty(Stack * stack);
+int Stack_popFront(Stack * stack);
+void Stack_pushFront(Stack * stack, int value);
+
+// ----------------------------------- Stack Sort...
+void stackSort(int * array, int len);
+
+// ----------------------------------- Generating 
+int isStackSortable(int * array, int len);
+void genShapes(int n);
+
+#endif
