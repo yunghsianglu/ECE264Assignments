eb80cbe6468c6e2991a6e18ec70ac82ff1d8115b
Abraham Budiman <abudiman@ecelinux02.ecn.purdue.edu>
2013-11-06 11:22:31 -0500

    add the structure for the stack

--- Fall2013/PA09/pa09.h
+++ Fall2013/PA09/pa09.h
@@ -1,1 +1,16 @@
+#ifndef PA09_H
+#define PA09_H
+
+typedef struct _huffnode {
+    int value; 
+    struct _huffnode * left;
+    struct _huffnode * right;
+} HuffNode;
+
+typedef struct _stack {
+    struct _stack * next;
+    HuffNode * node;
+} Stack;
+
+#endif
 
