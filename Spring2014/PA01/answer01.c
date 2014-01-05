ee0e5bd1ed14fa380c5931e2255da22804454be8
Aaron Michaux <aaron@pageofswords.net>
2014-01-04 19:15:39 -0500

    Createing spring-2014, pa01

---
+++ Spring2014/PA01/answer01.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "answer01.h"
+
+int arraySum(int * array, int len)
+{
+    return 0;
+}
+
+int arrayCountNegative(int * array, int len)
+{
+    return 0;
+}
+
+int arrayIsIncreasing(int * array, int len)
+{    
+    return 0;
+}
+
+int arrayIndexRFind(int needle, const int * haystack, int len)
+{
+    return 0;
+}
+
+int arrayFindSmallest(int * array, int len)
+{
+    return 0;
+}
